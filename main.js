f65e24293873dcd8d3ebabc4e4c47901a1392859
IT-SNK <saktaganauryz@gmail.com>
2022-01-26 21:27:51 +0300

    шпаргалки по js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict"
+"use strict"
 // let answer = prompt("Зимой и летом одним цветом");
 
 // if (answer.toLowerCase() == "елка"){
@@ -7,14 +8,14 @@
 //     alert("xuilo")
 // }
 
-//3primer 
+//3primer
 // let name = prompt("ymya");
 // let surname = prompt("famil");
 //                                                              // alert('privet,' + name + '.fami your' + surname);
 // if (name.length > 2 && surname.length > 2){
 // alert(`privet ${name}  ${surname}`);
 // } else{
-//    alert ("xuilo");            
+//    alert ("xuilo");
 // }
 
 //4 primer
@@ -38,11 +39,11 @@
 //     case "Понедельник":
 //         alert("будем пахать");//console.log тоже канает
 //         break;
-    
+
 //     case "вторник":
 //         alert("будем жариить баб");
 //         break;
-        
+
 //     default:
 //         alert("xuilo");
 
@@ -51,8 +52,50 @@
 //6 primer shablon
 // let name = prompt("Введите имя");
 // let surname = prompt("Введите фамилию");
-                                                            
-// if (name.length > 2 && surname.length > 2){
-// alert(`privet ${name}  ${surname}`);
+
+// if (name.length > 2 && surname.length > 2) {
+//     alert(`privet ${name}  ${surname}`);
 // }
 
+
+
+//7 primer
+// let i = 0;
+// while (i < 5) {
+//     console.log("какой то " + i);
+//     i++
+// }
+
+
+//8 primer
+// for (let i = 0; i < 5; i++) {
+//     alert("xuilo " + i)
+// }
+
+
+//9 primer(бесконечный цикл)
+// while (true) {
+//     let number = +prompt("веди число мразь");
+//     if (number > 0) {
+//         break;
+//     }
+// }
+
+//10 primer
+// for (let i = 0; i < 5; i++) {
+//     if (i == 3) {
+//         continue;
+//     } console.log(i);
+// }
+
+//11 primer //massiv
+// const array = [5, 7, 9];
+// console.log(array);
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+
+//12primer
+let users = ['Жанат', 'Батыр', 'Акака'];
+alert('Мой сын :' + users[1]);                           //console.log(users);
+alert('Жена: ' + users[0]);
