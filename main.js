1f9690895aeb8336ba8f2aa7abb630e4a98ff905
IT-SNK <saktaganauryz@gmail.com>
2022-01-26 13:40:07 +0300

    создал шаблон по js урокам

---
+++ main.js
@@ -0,0 +1,58 @@
+"use strict"
+// let answer = prompt("Зимой и летом одним цветом");
+
+// if (answer.toLowerCase() == "елка"){
+//     alert("правильно");
+// }else {
+//     alert("xuilo")
+// }
+
+//3primer 
+// let name = prompt("ymya");
+// let surname = prompt("famil");
+//                                                              // alert('privet,' + name + '.fami your' + surname);
+// if (name.length > 2 && surname.length > 2){
+// alert(`privet ${name}  ${surname}`);
+// } else{
+//    alert ("xuilo");            
+// }
+
+//4 primer
+// let age = Number(prompt("Введите ваш возраст"));
+
+// if(age < 18) {
+//     alert("вам скидка 10%");
+// } else if (age >= 18 && age < 60) {
+//     alert("Вам скидка 20%");
+// } else if (age >= 60) {
+// alert ("вам скидка 30%");
+// }else {
+//     alert ("xuilo");
+// }
+
+
+
+//5primer
+// let day = prompt("Ведите день недели");
+// switch(day) {
+//     case "Понедельник":
+//         alert("будем пахать");//console.log тоже канает
+//         break;
+    
+//     case "вторник":
+//         alert("будем жариить баб");
+//         break;
+        
+//     default:
+//         alert("xuilo");
+
+// }
+
+//6 primer shablon
+// let name = prompt("Введите имя");
+// let surname = prompt("Введите фамилию");
+                                                            
+// if (name.length > 2 && surname.length > 2){
+// alert(`privet ${name}  ${surname}`);
+// }
+
